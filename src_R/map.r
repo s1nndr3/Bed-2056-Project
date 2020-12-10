fded4386d913a0b0bb18d07537c6a2f07a4b2c87
sso149 <sso149@post.uit.no>
2020-12-10 04:12:50 +0100

    progress

--- src_R/map.r
+++ src_R/map.r
@@ -1,5 +1,6 @@
 library("maps")
 library(plotly)
+library("ggmap")
 
 map <- map_data("world")
 
@@ -18,3 +19,21 @@ ggplot() + geom_polygon(data = map, aes(x=long, y = lat, group = group)) +
   #geom_sf() +
   #coord_sf(xlim = c(-102.15, -74.12), ylim = c(7.65, 33.97), expand = FALSE)
   coord_fixed(1.3, xlim = c(4, 11.9100), ylim = c(49, 59.1852), expand = FALSE)
+
+
+get_googlemap("stavanger norway", zoom = 12, maptype = "satellite") %>% ggmap()
+
+library(leaflet)
+map3 <- leaflet() %>%
+addTiles() #%>%  # Add default OpenStreetMap map tiles
+for(n in 1:n_places){
+  temp <- loc_parking[n,]
+  map3 <- map3 %>% addMarkers(lat=temp$Lat, lng=temp$Lon, popup=temp$Place)
+}
+
+
+# map3$setView(c(59.914, 10.72), zoom = 13) # Longitude and Latitude of Oslo center - 59.9140100;10.7246670
+# map3$marker(c(59.9140100,10.7246670), bindPopup = "<p> Hi. I am a popup </p>")
+# map3$marker(c(59.9085200,10.7645700), bindPopup = "<p> Hello Oslo! </p>")
+# map3$save('Oslo.html', 'inline', cdn=TRUE)
+# cat('<iframe src="Oslo.html" width=100%, height=600></iframe>')
---
+++ src_R/regression.r
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(lubridate)
+library(plotly)
+library(dplyr)
+
+make_regression <- function(data, name){
+  Air_temp_fig <- na.omit(data) %>%  mutate(X = get(name)) %>%
+    plot_ly(x = ~X) %>% 
+    add_markers(y = ~Siddis, name = "Siddis", type = "scatter") %>% 
+    add_trace(y = ~fitted(lm(Siddis ~ X)), name = "Siddis Trace", type = "scatter", mode = "line") %>% 
+    add_markers(y = ~Forum, name = "Forum", visible = F) %>%
+    add_trace(y = ~fitted(lm(Forum ~ X)), name = "Forum Trace", type = "scatter", mode = "line", visible = F) %>% 
+    add_markers(y = ~Jernbanen, name = "Jernbanen", visible = F) %>%
+    add_trace(y = ~fitted(lm(Jernbanen ~ X)), name = "Jernbanen Trace", type = "scatter", mode = "line", visible = F) %>% 
+    add_markers(y = ~Jorenholmen, name = "Jorenholmen", visible = F) %>%
+    add_trace(y = ~fitted(lm(Jorenholmen ~ X)), name = "Jorenholmen Trace", type = "scatter", mode = "line", visible = F) %>% 
+    add_markers(y = ~Kyrre, name = "Kyrre", visible = F) %>%
+    add_trace(y = ~fitted(lm(Kyrre ~ X)), name = "Kyrre Trace", type = "scatter", mode = "line", visible = F) %>% 
+    add_markers(y = ~Parketten, name = "Parketten", visible = F) %>%
+    add_trace(y = ~fitted(lm(Parketten ~ X)), name = "Parketten Trace", type = "scatter", mode = "line", visible = F) %>% 
+    add_markers(y = ~Posten, name = "Posten", visible = F) %>%
+    add_trace(y = ~fitted(lm(Posten ~ X)), name = "Posten Trace", type = "scatter", mode = "line", visible = F) %>% 
+    add_markers(y = ~`St Olav`, name = "St Olav", visible = F) %>%
+    add_trace(y = ~fitted(lm(`St Olav` ~ X)), name = "St Olav Trace", type = "scatter", mode = "line", visible = F) %>% 
+    add_markers(y = ~Valberget, name = "Valberget", visible = F) %>%
+    add_trace(y = ~fitted(lm(Valberget ~ X)), name = "Valberget Trace", type = "scatter", mode = "line", visible = F) %>%
+    layout(
+      title = (list(text = paste("Parking vs", name), x = 0.1)),
+      xaxis = list(title = name),
+      yaxis = list(title = "Available parking spaces"),
+      updatemenus = list(
+        list(
+          y = 1,
+          x = 0,
+          buttons = list(
+            list(method = "restyle",
+                 args = list("visible", list(TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)),
+                 label = "Siddis"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)),
+                 label = "Forum"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)),
+                 label = "Jernbanen"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)),
+                 label = "Jorenholmen"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)),
+                 label = "Kyrre"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)),
+                 label = "Parketten"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE)),
+                 label = "Posten"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE)),
+                 label = "St Olav"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE)),
+                 label = "Valberget"),
+            
+            list(method = "restyle",
+                 args = list("visible", list(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)),
+                 label = "All")
+          )
+        )
+      )
+    )
+  return(Air_temp_fig)
+}
